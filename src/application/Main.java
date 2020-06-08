65c5a74b58335268c02d346f85d0c0dc66ceac3c
gabriel <g.dos.pinheiro1999@gmail.com>
2020-06-07 23:38:20 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,33 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+
+			// chamar o arquivo através de uma instância permite que manipular a tela antes
+			// de carregar.
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			stage.setScene(mainScene);
+			stage.setTitle("Sample example for main view");
+
+			stage.show();
+		}
+
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
